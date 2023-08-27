b1f97af5a82794b2523bfbef6b0e7bb002ef8296
Al-Deen <aldeen298666@gmail.com>
2023-08-27 16:57:00 +0600

    React Project basic setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaYoutube } from 'react-icons/fa';
+
+const App = () => {
+    return (
+        <div>
+            <header>
+                <h1>A template for React Project</h1>
+            </header>
+            <main>
+                <h1>Welcome to React</h1>
+            </main>
+            <footer>
+        
+                <p>Developed with &hearts; by SAD </p>
+            </footer>
+        </div>
+    );
+};
+
+export default App;
